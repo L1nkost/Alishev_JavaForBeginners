733cde377f91e08ea6e96beb6396f4ed17d5422d
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-14 21:36:14 +0300

    added prac 7 & 8

---
+++ src/Alishev_lessons/practice_less7.java
@@ -0,0 +1,13 @@
+package Alishev_lessons;
+
+import java.util.Scanner;
+
+public class practice_less7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter something");
+        String str = sc.nextLine();
+
+    }
+}
---
+++ src/Alishev_lessons/practice_less8.java
@@ -0,0 +1,17 @@
+package Alishev_lessons;
+
+import java.util.Scanner;
+
+public class practice_less8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        do {
+            System.out.println("enter correct number");
+            num = sc.nextInt();
+        } while ( num != 1337);
+    }
+}
