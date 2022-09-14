76e8f0acabc04aaeb655fffe6752153b6a44fb35
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-14 21:20:21 +0300

    added code

--- src/Alishev_lessons/practice_less6.java
+++ src/Alishev_lessons/practice_less6.java
@@ -1,4 +1,24 @@
 package Alishev_lessons;
 
+import java.util.Scanner;
 public class practice_less6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number between 0 and 100");
+
+        int point = sc.nextInt();
+
+        if ( point < 42 ) {
+            point = point + 16;
+            System.out.println(point);
+        }
+        else if ( point > 42 ) {
+            point = point - 10;
+            System.out.println(point);
+        }
+        else if ( point == 42 ) {
+            System.out.println("point");
+        }
+    }
 }
