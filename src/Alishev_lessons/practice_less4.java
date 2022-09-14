3e208edd594a58e8c3abdb94fb7dd2c2e6d03d58
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-14 20:41:26 +0300

    added practice_less4.java

---
+++ src/Alishev_lessons/practice_less4.java
@@ -0,0 +1,14 @@
+package Alishev_lessons;
+
+public class practice_less4 {
+    public static void main(String[] args) {
+
+        int value = 13;
+
+        while ( value <= 69 ) {
+            value = value + 5;
+            System.out.println(value);
+        }
+        System.out.println("you're off the limit");
+    }
+}
