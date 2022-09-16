9f20521ddcefe81258efb4c9b61806e1d8d39188
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-16 19:40:30 +0300

    added prac 11

---
+++ src/Alishev_lessons/practice_less11.java
@@ -0,0 +1,11 @@
+package Alishev_lessons;
+
+public class practice_less11 {
+    public static void main(String[] args) {
+        int[] array = new int[13];
+        for ( int i = 0; i<array.length; i++ ) {
+            array[i] = i * 5;
+            System.out.println(array[i]);
+        }
+    }
+}
