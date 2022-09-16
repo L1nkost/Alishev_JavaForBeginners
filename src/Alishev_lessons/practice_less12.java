c60cf38a39a679db6eff23a0ec24fa2e285cbc85
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-16 19:40:38 +0300

    added prac 12

---
+++ src/Alishev_lessons/practice_less12.java
@@ -0,0 +1,19 @@
+package Alishev_lessons;
+
+public class practice_less12 {
+
+    public static void main(String[] args) {
+
+        String[] s = new String[6];
+        s[0] ="Hi ";
+        s[1] ="my ";
+        s[2] ="name ";
+        s[3] ="is ";
+        s[4] ="Walter ";
+        s[5] ="White ";
+
+        for (String f:s) {
+            System.out.print(f);
+        }
+    }
+}
