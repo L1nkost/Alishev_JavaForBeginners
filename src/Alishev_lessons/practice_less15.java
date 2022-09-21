a51a152c9dda73ceaa4cda7a30f42c29300912ea
KanstantsinPilipenko <kostan2003@gmail.com>
2022-09-21 20:32:33 +0300

    Refactoring

--- src/Alishev_lessons/practice_less15.java
+++ src/Alishev_lessons/practice_less15.java
@@ -1,31 +1,56 @@
 package Alishev_lessons;
 
+import com.ibm.dtfj.corereaders.zos.mvs.LsedTemplate;
+
 public class practice_less15 {
     public static void main(String[] args) {
 
         Person2 h1 = new Person2();
-        h1.name = "dude";
-        h1.age = 27;
-        h1.sayHello();
-
-        System.out.println( "Hi " +h1.name+ ", how you live in " +h1.age+ "?" );
-
-        Person2 h2 = new Person2();
-        h2.age = 13;
-        h2.name = "bob";
-        h2.speak();
-
-        System.out.println( "I'm " +h2.name+ ", just living life. I'm " +h2.age+ " now. It sucks.");
-
+        h1.setName("Jake");
+        h1.setAge(34);
+        System.out.println("Выводим значение в мейн методе: " +h1.getAge());
+        System.out.println("Выводим значение в мейн методе: " +h1.getName());
     }
 }
 
     class Person2 {
-        String name;
-        int age;
+        private String name;
+        private int age;
+
+        public void setName(String Username) {
+            if (Username.isEmpty()) {
+                System.out.println("Empty name");
+            } else {
+                name = Username;
+            }
+        }
+        public String getName() {
+            return name;
+        }
+
+        public void setAge(int Userage) {
+            if (Userage < 0) {
+                System.out.println("Empty age");
+            } else {
+                age = Userage;
+            }
+        }
+        public int getAge() {
+            return age;
+        }
+
+        void setNameAndAge(String username, int userage) {
+            name = username;
+            age = userage;
+        }
+
+        int calculateYearsToRetirement() {
+            int years = 65-age;
+            return years;
+        }
 
         void speak() {
-            System.out.println( "Hi, i'm Jeff Bezos" );
+            System.out.println( "Hi, i'm " +name );
         }
         void sayHello() {
             System.out.println( "Hello" );
