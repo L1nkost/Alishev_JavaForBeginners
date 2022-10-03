f213d578be6c7d34898a6b21c732ac01ef0c63b1
KanstantsinPilipenko <kostan2003@gmail.com>
2022-10-04 01:22:38 +0300

    added less 27

---
+++ src/Alishev_lessons/Test.java
@@ -0,0 +1,13 @@
+package Alishev_lessons;
+
+import Forest.*;  // "*" импортируёт всё с этого пакета
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Squirell sq1 = new Squirell();
+        Tree t1 = new Tree();
+    }
+}
