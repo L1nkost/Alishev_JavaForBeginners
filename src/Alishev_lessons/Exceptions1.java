25c03c6027126f2651698d8f5ddc3b4bcbd0a7e0
KanstantsinPilipenko <kostan2003@gmail.com>
2022-10-08 18:43:51 +0300

    added less exceptions1

---
+++ src/Alishev_lessons/Exceptions1.java
@@ -0,0 +1,20 @@
+package Alishev_lessons;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exceptions1 {
+    public static void main(String[] args) {
+        readFile();
+    }
+
+    public static void readFile() {
+        File file = new File("Test6");
+        try {
+            Scanner scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("in method main");
+        }
+    }
+}
