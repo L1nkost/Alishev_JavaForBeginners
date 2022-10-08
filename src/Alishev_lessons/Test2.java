32c7def2d6a30528f0807addff61e37d44c2d295
KanstantsinPilipenko <kostan2003@gmail.com>
2022-10-08 15:31:20 +0300

    added less up & downcasting

---
+++ src/Alishev_lessons/Test2.java
@@ -0,0 +1,9 @@
+package Alishev_lessons;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Dog dog = (Dog)a;
+        dog.bark();
+    }
+}
--- src/Alishev_lessons/less25.java
+++ src/Alishev_lessons/less25.java
@@ -4,6 +4,5 @@ public class less25 {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.eat();
-        dog.showName();
     }
 }
