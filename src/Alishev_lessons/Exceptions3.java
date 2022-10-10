f6630a6e4c1070a7143982dcb4e512e88e5d98f5
KanstantsinPilipenko <kostan2003@gmail.com>
2022-10-11 01:36:01 +0300

    added less Exceptions 3

---
+++ src/Alishev_lessons/Exceptions3.java
@@ -0,0 +1,8 @@
+package Alishev_lessons;
+
+public class Exceptions3 {
+    public static void main(String[] args) {
+        // Checked Exceptin(Ошибка сразу)
+        // Uncheckes Exception(Ошибка при компиляции)
+    }
+}
