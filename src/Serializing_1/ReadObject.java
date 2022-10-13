af7363041ee4e20f2a9788a7fffeafeee042a282
KanstantsinPilipenko <kostan2003@gmail.com>
2022-10-13 23:43:00 +0300

    added less 46 serializing 2

--- src/Serializing_1/ReadObject.java
+++ src/Serializing_1/ReadObject.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) throws FileNotFoundException {
@@ -11,11 +12,9 @@ public class ReadObject {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
+            Person[] people = (Person[]) ois.readObject();
 
-            System.out.println(person1);
-            System.out.println(person2);
+            System.out.println(Arrays.toString(people));
 
             ois.close();
         } catch (IOException e) {
